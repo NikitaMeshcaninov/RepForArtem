06adaef5e90e6445ae241fbe0bd58644fc9775fe
NIkita Meshchaninov <meshn@outlook.com>
2016-07-06 17:35:20 +0300

    Заменил слипы на вейты

--- src/test/java/GameGnomClassTest.java
+++ src/test/java/GameGnomClassTest.java
@@ -10,11 +10,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
 import java.sql.Driver;
+import java.util.concurrent.TimeUnit;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
@@ -36,43 +40,47 @@ public class GameGnomClassTest {
 
     @After
     public void saveScreenshotAndCloseBrowser() throws IOException, InterruptedException {
+
+        final Wait<WebDriver> wait = new WebDriverWait(driver, 5).ignoring(StaleElementReferenceException.class, ElementNotVisibleException.class);
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         screenshotHelper.saveScreenshot("screenshot.png");
         WebElement mainMenuButton = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[1]/h4/a"));
         mainMenuButton.click();
-        Thread.sleep(500);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[2]/ul/li[10]/a"))));
         WebElement myCharactersButton = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[2]/ul/li[10]/a"));
         myCharactersButton.click();
-        Thread.sleep(500);
         driver.switchTo().alert().accept();
-        Thread.sleep(500);
         WebElement moreForCharacterButton = driver.findElement(By.xpath("/html/body/div/div[1]/table/tbody/tr[4]/td[2]/div/button"));
         moreForCharacterButton.click();
-        Thread.sleep(500);
         WebElement xDellButton = driver.findElement(By.xpath("/html/body/div/div[1]/table/tbody/tr[4]/td[2]/div/ul/li[3]/a"));
         xDellButton.click();
-        Thread.sleep(1500);
         driver.close();
     }
 
     @Test
     public void gameTest() throws IOException, InterruptedException {
+
+        final Wait<WebDriver> wait = new WebDriverWait(driver, 5).ignoring(StaleElementReferenceException.class, ElementNotVisibleException.class);
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         assertEquals("The page title should e qual Google at the start of the test.", "Вход", driver.getTitle());
         WebElement enterField = driver.findElement(By.name("form-username"));
         enterField.sendKeys("shmublon");
-        Thread.sleep(500);
         WebElement enterButton = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div[2]/form/button"));
         enterButton.click();
-        Thread.sleep(500);
         WebElement nameField = driver.findElement(By.id("name"));
         nameField.sendKeys("Тестовый персонаж");
         Select raceSelector = new Select(driver.findElement(By.id("race_id")));
         raceSelector.selectByVisibleText("Гном");
         WebElement experienceField = driver.findElement(By.id("experience"));
         experienceField.sendKeys("200");
-        Thread.sleep(500);
         WebElement addCharacterButton = driver.findElement(By.xpath("/html/body/div/div[2]/form/div[5]/div/button"));
         addCharacterButton.click();
-        Thread.sleep(3000);
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/html/body/div/h3"))));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/html/body/div/div[3]/div[1]/div[2]/p"))));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/html/body/div/form/div/div[2]/div/p/strong"))));
 
         assertEquals("Name should be \"Тестовый персонаж\"", "тестовый персонаж",
                 driver.findElement(By.xpath("/html/body/div/h3")).getText().toLowerCase());
