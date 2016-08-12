93f441e3e5ac17bdd7f463ea8ade463e6ba229c3
NIkita Meshchaninov <meshn@outlook.com>
2016-08-12 15:10:35 +0300

    works

--- src/main/java/WebPages/HomePage.java
+++ src/main/java/WebPages/HomePage.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.support.PageFactory;
  * Created by Nikita on 18.07.2016.
  */
 public class HomePage {
-    private WebDriver driver;
     private static String pageURL = "http://erilon-staging.herokuapp.com/";
     @FindBy(how = How.XPATH, using = ".//*[@id='nickName']")
     private WebElement nickNameTextField;
@@ -18,7 +17,6 @@ public class HomePage {
     private WebElement loginButton;
 
     public HomePage(WebDriver driver) {
-        this.driver = driver;
         driver.get(pageURL);
         PageFactory.initElements(driver, this);
     }
