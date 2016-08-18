3c279e7b07fa2be1341c34bb7836fc6bd95afe2f
NIkita Meshchaninov <meshn@outlook.com>
2016-08-18 17:59:15 +0300

    Works but only sometimes

--- src/test/java/GameGnomClassTest.java
+++ src/test/java/GameGnomClassTest.java
@@ -105,10 +105,11 @@ public class GameGnomClassTest {
         personagePage.decreaseStaminaBy1();
         Thread.sleep(500);
         personagePage.openPropertisMenu();
+        System.out.println("1");
         wait.until(ExpectedConditions.visibilityOf(personagePage.getAddWorth()));
+        System.out.println("1.5");
         Utill search = new Utill();
-
-
+        System.out.println("2");
         assertEquals("У персонажа должна исчезнуть внушительность", false, search.isElementPresent
                 ("//*[contains(text(), 'Внушительность')]",driver));
         personagePage.savePersonage();
@@ -122,7 +123,6 @@ public class GameGnomClassTest {
         wait.until(ExpectedConditions.visibilityOf(personagePage.getAddWorth()));
         assertEquals("У персонажа должна исчезнуть внушительность", false, search.isElementPresent
                 ("//*[contains(text(), 'Внушительность')]",driver));
-
     }
 
     @After
