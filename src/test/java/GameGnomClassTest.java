9b6ca995282460e4b79fc1e227b1530d49cc5c67
Nikita <meshn@outlook.com>
2016-09-13 19:17:07 +0300

    Works but only sometimes All in ENG New statChange() method

--- src/test/java/GameGnomClassTest.java
+++ src/test/java/GameGnomClassTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.beans.Visibility;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -47,8 +48,6 @@ public class GameGnomClassTest {
         wait.until(ExpectedConditions.visibilityOf(personageCreatePage.getPersonageExp()));
 
 
-
-
         assertEquals("Name should be " + SettingsForTest.NAME, SettingsForTest.NAME,
                 personageCreatePage.getPersonageName().getText());
         assertEquals("Race of personage should be " + SettingsForTest.NAME.toLowerCase(), SettingsForTest.RACE.toLowerCase()
@@ -71,92 +70,96 @@ public class GameGnomClassTest {
 
         assertEquals("Warning should be present", "Требования не выполнены!"
                 , personageCreatePage.getWarningmesage().getText());
-        assertFalse("Button should not be active",  personageCreatePage.getAddButtonInPopupWindow().isEnabled());
+        assertFalse("Button should not be active", personageCreatePage.getAddButtonInPopupWindow().isEnabled());
 
 
         wait.until(ExpectedConditions.elementToBeClickable(personageCreatePage.getAbortAddWorthButton()));
         personageCreatePage.abortAddWorth();
-        LOGGER.info("Abort add worth operation");
+        LOGGER.info("1 Abort add worth operation");
         Utils.waitForInvisibility(driver, personageCreatePage.getAddWorthMenu());
         personageCreatePage.openCharacteristicsMenu();
-        LOGGER.info("Open character menu");
+        LOGGER.info("2 Open character menu");
         personageCreatePage.changeAttribute(Attribute.STRENGTH, INCREASE, 2); //strength, add 2 points
         personageCreatePage.changeAttribute(Attribute.VITALITY, INCREASE, 3); // stamina, add 3 points
-        LOGGER.info("Change hero stats to add worth");
+        LOGGER.info("3 Change hero stats to add worth");
         personageCreatePage.openPropertiesMenu();
-        LOGGER.info("Open hero properties menu");
+        LOGGER.info("4 Open hero properties menu");
         wait.until(ExpectedConditions.elementToBeClickable(personageCreatePage.getAddWorth()));
         personageCreatePage.clikAddWorth();
-        LOGGER.info("Open addWorth popup menu");
+        LOGGER.info("5 Open addWorth popup menu");
         wait.until(ExpectedConditions.elementToBeClickable(personageCreatePage.getWorthSelector()));
         personageCreatePage.selectWorth(SettingsForTest.WORTH);
-        LOGGER.info("Select worth to add");
+        LOGGER.info("6 Select worth to add");
         wait.until(ExpectedConditions.attributeContains(personageCreatePage.getLoader(), "aria-hidden", "true"));
-        assertFalse("No warning should be present",  personageCreatePage.getWarningmesage().isDisplayed());
-        assertTrue("Button should be active ",  personageCreatePage.getAddButtonInPopupWindow().isEnabled());
-        LOGGER.info("Check popup menu");
+        assertFalse("7 No warning should be present", personageCreatePage.getWarningmesage().isDisplayed());
+        assertTrue("Button should be active ", personageCreatePage.getAddButtonInPopupWindow().isEnabled());
+        LOGGER.info("8 Check popup menu");
         personageCreatePage.submitAddWorth();
-        LOGGER.info("Submitting worth add");
+        LOGGER.info("9 Submitting worth add");
         wait.until(ExpectedConditions.attributeContains(personageCreatePage.getLoader(), "aria-hidden", "true"));
-        assertTrue("Character should have worth",  personageCreatePage.getWorthImpressiveness().isDisplayed());
+        assertTrue("Character should have worth", personageCreatePage.getWorthImpressiveness().isDisplayed());
         personageCreatePage.savePersonage();
-        LOGGER.info("Check that worth was added, save character");
+        LOGGER.info("10 Check that worth was added, save character");
         wait.until(ExpectedConditions.attributeContains(personageCreatePage.getLoader(), "aria-hidden", "true"));
-        LOGGER.info("Refresh page");
+        LOGGER.info("11 Refresh page");
         driver.navigate().refresh();
         driver.switchTo().alert().accept();
         wait.until(ExpectedConditions.attributeContains(personageCreatePage.getLoader(), "aria-hidden", "true"));
-        LOGGER.info("Open properties menu");
+        LOGGER.info("12 Open properties menu");
         personageCreatePage.openPropertiesMenu();
         wait.until(ExpectedConditions.elementToBeClickable(personageCreatePage.getWorth()));
         personageCreatePage.openWorth();
-        LOGGER.info("Open worth menu");
+        LOGGER.info("13 Open worth menu");
         wait.until(ExpectedConditions.visibilityOf(personageCreatePage.getAddWorth()));
-        assertTrue("Worth of character should be saved",personageCreatePage.getWorthImpressiveness().isDisplayed());
+        assertTrue("Worth of character should be saved", personageCreatePage.getWorthImpressiveness().isDisplayed());
         personageCreatePage.openCharacteristicsMenu();
-        LOGGER.info("Check is worth saved, open characteristics menu");
-        LOGGER.info("Decrease str by 1");
+        LOGGER.info("14 Check is worth saved, open characteristics menu");
+        LOGGER.info("15 Decrease str by 1");
         personageCreatePage.changeAttribute(Attribute.STRENGTH, DECREASE, 1);
         personageCreatePage.openPropertiesMenu();
-        LOGGER.info("Open properties menu");
+        LOGGER.info("16 Open properties menu");
         wait.until(ExpectedConditions.attributeContains(personageCreatePage.getLoader(), "aria-hidden", "true"));
         //висит ровно 30 секунд, видимо не работатет
         assertFalse("Worth of character should disappeared", Utils.isElementCurrentlyPresent
                 ("//*[contains(text(), 'Внушительность')]", driver));
         personageCreatePage.savePersonage();
-        LOGGER.info("Check is worth Внушительность is disappeared");
+        LOGGER.info("17 Check is worth Внушительность is disappeared");
         wait.until(ExpectedConditions.attributeContains(personageCreatePage.getLoader(), "aria-hidden", "true"));
-        LOGGER.info("Refresh page");
+        LOGGER.info("18 Refresh page");
         driver.navigate().refresh();
         driver.switchTo().alert().accept();
         wait.until(ExpectedConditions.attributeContains(personageCreatePage.getLoader(), "aria-hidden", "true"));
-        LOGGER.info("Open prop menu");
+        LOGGER.info("19 Open prop menu");
         personageCreatePage.openPropertiesMenu(); // тут упало с ошибкой "Element is not
-                                                 // clickable at point (455, 607). Other
-                                                 // element would receive the click:
-                                                 // <div class="panel-group" id="perkAccordeon">...</div>"
+        // clickable at point (455, 607). Other
+        // element would receive the click:
+        // <div class="panel-group" id="perkAccordeon">...</div>"
         wait.until(ExpectedConditions.elementToBeClickable(personageCreatePage.getWorth()));
         personageCreatePage.openWorth();
-        LOGGER.info("Open worth menu");
+        LOGGER.info("20 Open worth menu");
         wait.until(ExpectedConditions.attributeContains(personageCreatePage.getLoader(), "aria-hidden", "true"));
         //висит ровно 30 секунд, видимо не работатет
-        assertFalse("У персонажа должна исчезнуть внушительность",  Utils.isElementCurrentlyPresent
+        assertFalse("У персонажа должна исчезнуть внушительность", Utils.isElementCurrentlyPresent
                 ("//*[contains(text(), 'Внушительность')]", driver));
-        LOGGER.info("End of test");
+        LOGGER.info("21 End of test");
     }
 
     @After
     public void delTestPersonageAndCloseBrowser() throws IOException, InterruptedException {
         final Wait<WebDriver> wait = new WebDriverWait(driver, 5);
-
         PersonageCreatePage personageCreatePage = new PersonageCreatePage(driver);
+
         personageCreatePage.openMainMenu();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(), 'персонажи')]")));
-
-
         PersonageListPage personageListPage = personageCreatePage.goToPersonageListPage();
-        personageListPage.openMoreMenuForPersonage();
-        personageListPage.delCharacter();
+
+
+        while (Utils.isElementCurrentlyPresent("//*[contains(text(), " + SettingsForTest.NAME + ")]", driver)) {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text()," + SettingsForTest.NAME + ")]")));
+            personageListPage.openMoreMenuForPersonage();
+            personageListPage.delCharacter();
+        }
+
         driver.close();
     }
 }
