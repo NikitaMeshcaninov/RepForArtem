92cc4b02eb3bf22045ea414050edeffe756ed144
Daria Khomenko <daria.khomenko@oracle.com>
2018-02-07 18:31:28 +0200

    My test task 5

--- src/test/java/Leilighem.java
+++ src/test/java/Leilighem.java
@@ -3,24 +3,24 @@ import org.apache.commons.lang3.ArrayUtils;
 import static org.apache.commons.lang3.ArrayUtils.add;
 
 /**
- * Created by dkhomenko on 05.02.2018.
+ * Created by dkhomenko on 07.02.2018.
  */
-class Leilighem {
-	public static void main(String[] args) {
-		int array[]  = new int[1];
-		for (int i = 0; i < array.length; i++) {
-			String toPrint = "array[" + i + "]=" + array[i];
+public class Leilighem {
+		public static void main(String[] args) {
+			int array[]  = new int[1];
+			for (int i = 0; i < array.length; i++) {
+				String toPrint = "array[" + i + "]=" + array[i];
 			/*System.out.print("Вывод начального массива: " + toPrint + "\n");*/
 
-			int randomNumber =  (int) (Math.random() * 10);
-			System.out.print("Вывод рандомного числа: " + randomNumber + "\n");
+				int randomNumber =  (int) (Math.random() * 10);
+				System.out.print("Вывод рандомного числа: " + randomNumber + "\n");
 
-			array = add(array, randomNumber);
+				array = add(array, randomNumber);
 
-			System.out.print("Вывод измененного массива: " + ArrayUtils.toString(array) + "\n");
-			if(array.length == 10) {
-				break;
+				System.out.print("Вывод измененного массива: " + ArrayUtils.toString(array) + "\n");
+				if(array.length == 10) {
+					break;
+				}
 			}
 		}
 	}
-}
