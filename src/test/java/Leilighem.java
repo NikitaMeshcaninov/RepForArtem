6cf3f8643cc60b145b50a671f9fb07dd81ac6d8b
Daria Khomenko <daria.khomenko@oracle.com>
2018-02-13 12:46:27 +0200

    My test task 7

--- src/test/java/Leilighem.java
+++ src/test/java/Leilighem.java
@@ -8,17 +8,23 @@ import static org.apache.commons.lang3.ArrayUtils.add;
  * Created by dkhomenko on 07.02.2018.
  */
 public class Leilighem {
-		public static void main(String[] args) {
-			int array[]  = new int[10];
-			for (int i = 0; i < array.length; i++) {
 
-				int randomNumber = new Random().nextInt(10);
-
-				System.out.print("Вывод рандомного числа: " + randomNumber + "\n");
-
-				array[i] = randomNumber;
-
-				System.out.print("Вывод измененного массива: " + ArrayUtils.toString(array) + "\n");
-			}
+	static int[] randomArray(int size) {
+		int array[] = new int[size];
+		for (int i = 0; i < array.length; i++) {
+			int randomNumber = new Random().nextInt(10);
+			array[i] = randomNumber;
 		}
+		return array;
 	}
+
+
+	public static void main(String[] args) {
+
+		Personage Huan = new Personage("half-human", "monk");
+
+		System.out.print("Вывод измененного массива: " + ArrayUtils.toString(randomArray(10)) + "\n");
+
+	}
+
+}
